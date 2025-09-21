9b138e7bdae63d5b3d75abecb1e68e27d1079900
Gan Shun Lim <ganshun@gmail.com>
2025-09-21 10:18:20 +0800

    Updating config to work for mac

--- home-manager/darwin/default.nix
+++ home-manager/darwin/default.nix
@@ -1,6 +1,22 @@
 { config, pkgs, ... }:
 
 {
-  import [ ./dock ];
+  imports = [ ./dock ];
   home.homeDirectory = "/Users/ganshun";
+  local = {
+    dock.username = "${config.home.username}";
+    dock.enable = true;
+    dock.entries = [
+      { path = "/System/Applications/Messages.app/"; }
+      { path = "/Applications/Google Chrome.app/"; }
+      { path = "${config.home.homeDirectory}/.nix-profile/Applications/Alacritty.app/"; }
+      { path = "${config.home.homeDirectory}/.nix-profile/Applications/Firefox.app/"; }
+      { path = "${config.home.homeDirectory}/"; options = "--sort name --view grid --display folder";}
+      # {
+      #   path = "${config.users.users.ganshun.home}/";
+      #   section = "others";
+      #   options = "--sort name --view grid --display folder";
+      # }
+    ];
+  };
 }
--- home-manager/darwin/dock/default.nix
+++ home-manager/darwin/dock/default.nix
@@ -9,22 +9,6 @@ let
 in
 {
   options = {
-  local = {
-    dock.username = "${config.home.username}";
-    dock.enable = true;
-    dock.entries = [
-      { path = "/System/Applications/Messages.app/"; }
-      { path = "/Applications/Google Chrome.app/"; }
-      { path = "${config.home.homeDirectory}/.nix-profile/Applications/Alacritty.app/"; }
-      { path = "${config.home.homeDirectory}/.nix-profile/Applications/Firefox.app/"; }
-      { path = "${config.home.homeDirectory}/"; options = "--sort name --view grid --display folder";}
-      # {
-      #   path = "${config.users.users.ganshun.home}/";
-      #   section = "others";
-      #   options = "--sort name --view grid --display folder";
-      # }
-    ];
-  };
     local.dock = {
       enable = mkOption {
         description = "Enable dock";
